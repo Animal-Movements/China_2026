cccc78efb7651bcdbcbf626204f94156b7e544d6
Jared Stabach <jstabach@users.noreply.github.com>
2025-08-08 01:18:59 -0400

    Leaving in the pptx and export to R files

---
+++ 1_Day1_DataCleaning/Export_to_R.R
@@ -0,0 +1,4 @@
+# extract R code
+library(knitr)
+file.exists("1_DataCleaning.Rmd")
+purl("1_DataCleaning.Rmd")
---
+++ 3_Day3_CovariateExtraction/Export_to_R.R
@@ -0,0 +1,5 @@
+# extract R code
+library(knitr)
+file.exists("3_HMMs.Rmd")
+purl("3_HMMs.Rmd")
+
